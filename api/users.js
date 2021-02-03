8c71e1adeb923680d124d8b3e059aa9887d09a0e
unix <unix.bio@gmail.com>
2021-02-03 22:26:21 +0800

    fix: fix organization name

--- api/users.js
+++ api/users.js
@@ -28,7 +28,7 @@ const filterContributors = data => {
 
 const getContributors = async (path, repo) => {
   const query = `query($path: String!, $repo: String!) {
-    repository(owner: "geist-ui", name: $repo) {
+    repository(owner: "geist-org", name: $repo) {
       object(expression: "master") {
         ... on Commit {
           history(first: 100, path: $path) {
